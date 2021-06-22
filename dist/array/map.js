d66d2d261496a61f61c2e7bb4b18984302b6e423
Nitin Bansal <1055173+nitinbansal@users.noreply.github.com>
2021-06-23 00:13:57 +0530

    map fix

--- dist/array/map.js
+++ dist/array/map.js
@@ -1,14 +1,14 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-function map(arr, handler, count) {
+async function map(arr, handler, count) {
     count = count || arr.length;
     const workersCount = Math.max(Math.floor(Math.min(count, arr.length)), 0);
-    const results = [];
-    Array.from({ length: workersCount }).forEach((w, workerIndex) => {
+    let results = [];
+    await Promise.all(Array.from({ length: workersCount }).map(async (w, workerIndex) => {
         for (let i = workerIndex; i < arr.length; i += workersCount) {
-            results[i] = handler(arr[i], i);
+            results[i] = await handler(arr[i], i);
         }
-    });
+    }));
     return results;
 }
 exports.default = map;
--- src/array/map.ts
+++ src/array/map.ts
@@ -1,18 +1,18 @@
-function map<T, R>(
+async function map<T, R>(
 	arr: T[],
 	handler: (ele: T, index: number) => Promise<R>,
 	count?: number,
-): Promise<R>[] {
+): Promise<R[]> {
 	// Cap workers count to task list size, with a min of 1 worker
 	count = count || arr.length;
 	const workersCount = Math.max(Math.floor(Math.min(count, arr.length)), 0);
 
-	const results: Promise<R>[] = [];
-	Array.from({ length: workersCount }).forEach((w, workerIndex) => {
+	let results: R[] = [];
+	await Promise.all(Array.from({ length: workersCount }).map(async (w, workerIndex) => {
 		for (let i = workerIndex; i < arr.length; i += workersCount) {
-			results[i] = handler(arr[i], i);
+			results[i] = await handler(arr[i], i);
 		}
-	});
+	}));
 	return results;
 }
 
